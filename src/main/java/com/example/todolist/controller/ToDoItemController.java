6ae12213fc2976b134eaa9d3c9a5c4afe904b161
VietHLe <87550192+VietHLe@users.noreply.github.com>
2021-07-23 18:20:34 -0400

    third commit

--- src/main/java/com/example/todolist/controller/ToDoItemController.java
+++ src/main/java/com/example/todolist/controller/ToDoItemController.java
@@ -3,14 +3,10 @@ package com.example.todolist.controller;
 
 import com.example.todolist.model.ToDoItem;
 import com.example.todolist.service.ToDoItemService;
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.List;
 
 @RestController
